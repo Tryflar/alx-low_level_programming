5b8aa79770d7a3fecbaee83e29e79a8823d6de03
Tryflar <okotheugene@gmail.com>
2023-07-19 05:10:36 -0700

    Debugg

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,12 +8,15 @@
 int main(void)
 {
 	int 1;
+
 	printf("Infinite loop incoming :(\n");
-	1=0;
+	i = 0;
+
 	/*while (i < 10)*/
 	/*{*/
 	/*putchar(i);*/
 	/*}*/
 	printf("Infinite loop avoided! \\o/n");
+
 return (0);
 }
